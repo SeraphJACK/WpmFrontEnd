57c031b4faffa7d61896fbca4fd5d1b90e664ee0
SeraphJACK <seraphjack@outlook.com>
2020-10-07 11:28:10 +0800

    Add search

---
+++ js/list.js
@@ -0,0 +1,54 @@
+// Waypoint List
+function toggleList() {
+  if (document.getElementById('list').classList.contains('hide')) {
+    updateList();
+    show('list');
+  } else {
+    hide('list');
+  }
+}
+
+let list = document.getElementById('list-content');
+
+function updateList() {
+  clearList();
+  let query = document.getElementById('search').value;
+  let wps = waypoints.map.values();
+  let wp
+  while (!(wp = wps.next()).done) {
+    if (wp.value.available && wp.value.name.toLowerCase().includes(query.toLowerCase())) {
+      appendList(wp.value);
+    }
+  }
+}
+
+function clearList() {
+  while (list.firstChild) list.lastChild.remove();
+}
+
+function appendList(wp) {
+  let e = document.createElement("li");
+
+  let name = document.createElement("p");
+  name.innerText = wp.name;
+  e.appendChild(name);
+
+  let id = document.createElement("p");
+  id.innerText = wp.identifier;
+  e.appendChild(id);
+
+  e.id = wp.identifier;
+  e.onclick = () => handleListClick(e);
+  list.appendChild(e);
+}
+
+function handleListClick(e) {
+  let wp = waypoints.getI(e.id);
+  config.nowChunkX = wp.x >> 4;
+  config.nowChunkZ = wp.z >> 4;
+  config.offsetX = config.offsetZ = 0;
+  render();
+  hide('list');
+}
+
+document.getElementById('search').onchange = updateList
